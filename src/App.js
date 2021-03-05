556cb3d0210f9a1617ab8b49af556a5a3a568b87
lauren levaccare <laurenlevaccare@icloud.com>
2021-03-05 14:04:44 -0600

    linked up nav bar to components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 
 import './App.css';
-import Header from "./components/Header"
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import MenuListComposition from "./components/Header"
+import Contact from "./components/Contact"
+import About from "./components/About"
+import Projects from "./components/Projects"
+// import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <div className="container">
-      
-    <Header/>
-    </div>
-  );
-}
 
-export default App;
+  function App() {
+    return (
+
+      <Router>
+        <div className="container" >
+
+          <MenuListComposition />
+
+
+
+          <Route exact path="/"/>
+          
+          <Route exact path="/contact" component={Contact}/>
+          
+          <Route exact path="/about" component={About} />
+
+          <Route exact path="/projects" component={Projects}/>
+        
+
+          
+          
+        </div>
+      </Router>
+
+     
+
+    );
+  }
+
+  export default App;
+
